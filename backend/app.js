165cdbbff957599aab36fdd462d5a7602155ba7c
C-S26 <pavanprakash0909@gmail.com>
2025-06-19 20:16:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -87,4 +87,7 @@ app.post('/messages', async (req, res) => {
 
 // Start server
 connectDB();
-app.listen(5000, () => console.log('✅ Server running at http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
